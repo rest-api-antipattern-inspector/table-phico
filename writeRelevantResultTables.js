67ebfa3130eb3129c729a79ddbdd4d1ec635b90c
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-20 11:01:14 +0200

    js sorts

--- writeRelevantResultTables.js
+++ writeRelevantResultTables.js
@@ -4,9 +4,9 @@ const tables = JSON.parse(
   fs.readFileSync('./resultTables.json', 'utf8')
 )
 
-const relevantTables = tables.filter(
-  (t) => t.result < -0.2 || t.result > 0.2
-)
+const relevantTables = tables
+  .filter((t) => t.result < -0.2 || t.result > 0.2)
+  .sort((a, b) => a.result < b.result)
 
 fs.writeFileSync(
   './relevantResultTables.json',
